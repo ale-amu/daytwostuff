96eab28dec328e5ca9431eff590496c0737633f6
ale-amu <231544263+ale-amu@users.noreply.github.com>
2026-01-16 17:41:47 +0000

    First Lab Changes

---
+++ firstlab.py
@@ -0,0 +1,31 @@
+# %%
+print("/workspaces/daytwostuff") #terminal display path
+# Should you include the environment in your repo or not?
+print("No, do include your environment in your repo; not because anyone can make " \
+"their own using the requirements file and it takes up space"),  
+# %%
+print("/workspaces/daytwostuff") #terminal display path is the same
+# %%
+import pandas as pd
+data = pd.read_csv("NBAStats202223.csv")
+data
+# %%
+#What do you notice about the extension menu?
+print("It has a column if multiple extensions mainly associated with Microsoft. " \
+"Data Wrangler is shown as being credible due to the blue check mark on its left and" \
+" summary on its capabilities")
+
+# %%
+#Review the capabilities, what are three useful elements of Data Wrangler
+print("Data viewing, cleaning and preparation for tabular datasets")
+
+# %%
+print("6.5.1") #Note the version
+# %%
+#Why do we use a requirements.txt file?
+print("The requirements.txt file is used to ensure all packages and versions are " \
+"reproduceable on all devices while being held in a central hub; It can also be used " \
+"by others to recreate the same environment.")
+
+###
+
